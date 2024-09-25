d1fc9ecb2eb577bd1b0dcce6791dc84f33ee8aaa
Yedil Kulzhabay <edil.kulzhabay01@gmail.com>
2024-09-25 17:04:18 +0500

    userNameOrders

--- client/src/Pages/OrderList.js
+++ client/src/Pages/OrderList.js
@@ -562,7 +562,7 @@ export default function OrderList() {
                                             Заказ: (
                                             {item?.createdAt.slice(0, 10)})
                                         </div>
-                                        <div>{item?.client?.fullName}</div>
+                                        <div>{item?.client?.userName}</div>
                                         <a target="_blank" rel="noreferrer" href={item?.address?.link} className="text-blue-500 hover:text-green-500">{item?.address?.actual}</a>
                                         <div>{item?.date?.d} {item?.date?.time !== "" && item?.date?.time}</div>
                                         <div>{item?.products?.b12 !== 0 && `12.5л: ${item?.products?.b12}`}; {item?.products?.b19 !== 0 && `18.9л: ${item?.products?.b19}`}</div>
@@ -587,7 +587,7 @@ export default function OrderList() {
                                             Заказ: (
                                             {item?.createdAt.slice(0, 10)})
                                         </div>
-                                        <div>{item?.client?.fullName}</div>
+                                        <div>{item?.client?.userName}</div>
                                         <a target="_blank" rel="noreferrer" href={item?.address?.link} className="text-blue-500 hover:text-green-500">{item?.address?.actual}</a>
                                         <div>{item?.date?.d} {item?.date?.time !== "" && item?.date?.time}</div>
                                         <div>{item?.products?.b12 !== 0 && `12.5л: ${item?.products?.b12}`}; {item?.products?.b19 !== 0 && `18.9л: ${item?.products?.b19}`}</div>
