7b268c64e60fc24a3af0b5310dd8fff09c5e98b2
Yedil Kulzhabay <edil.kulzhabay01@gmail.com>
2024-10-05 18:43:42 +0500

    socket

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -287,7 +287,10 @@ export const updateOrder = async (req, res) => {
                 );
             }
             await order.save();
-            global.io.emit("orderStatusChanged", {
+            
+            const client = await Client.findById(order.client)
+            const clientId = client._id
+            global.io.to(clientId).emit("orderStatusChanged", {
                 orderId: order._id,
                 status: changeData,
                 message: `Статус заказа #${order._id} был изменен на ${changeData}`,
