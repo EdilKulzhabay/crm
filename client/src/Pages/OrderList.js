83dbeb72a578f29fd9f72cbdc33e755ca3003fb0
Yedil Kulzhabay <edil.kulzhabay01@gmail.com>
2024-10-15 20:28:35 +0500

    15.10.2024 date

--- client/src/Pages/OrderList.js
+++ client/src/Pages/OrderList.js
@@ -12,6 +12,16 @@ import clsx from "clsx";
 import OrderInfo from "../Components/OrderInfo";
 import useFetchUserData from "../customHooks/useFetchUserData";
 import useScrollPosition from "../customHooks/useScrollPosition";
+import DataInput from "../Components/DataInput";
+
+const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
+
 
 export default function OrderList() {
     const scrollPosition = useScrollPosition();
@@ -22,8 +32,8 @@ export default function OrderList() {
     const [searchF, setSearchF] = useState("");
     const [searchStatus, setSearchStatus] = useState(false);
     const [dates, setDates] = useState({
-        startDate: "",
-        endDate: "",
+        startDate: getCurrentDate(),
+        endDate: getCurrentDate(),
     });
     const [franchiseesModal, setFranchiseesModal] = useState(false);
     const [franchisee, setFranchisee] = useState(null);
@@ -50,6 +60,40 @@ export default function OrderList() {
         setFranchisee(chFranchisee);
         setFranchiseesModal(false);
     };
+
+    const handleDateChange = (e) => {
+        let input = e.target.value.replace(/\D/g, ""); // Remove all non-digit characters
+        if (input.length > 8) input = input.substring(0, 8); // Limit input to 8 digits
+
+        const year = input.substring(0, 4);
+        const month = input.substring(4, 6);
+        const day = input.substring(6, 8);
+
+        let formattedValue = year;
+        if (input.length >= 5) {
+            formattedValue += "-" + month;
+        }
+        if (input.length >= 7) {
+            formattedValue += "-" + day;
+        }
+
+        setDates({ ...dates, [e.target.name]: formattedValue });
+    };
+
+    const handleDate = () => {
+        if (dates.startDate.length !== 10 || dates.endDate.length !== 10) {
+            setOpen(true)
+            setStatus("error")
+            setMessage("Введите даты в формате ГГГГ-ММ-ДД")
+            return
+        }
+        
+        setOrders([]);
+        setPage(1);
+        setHasMore(true);
+        setLoading(false)
+        loadMoreOrders(1, dates, search, searchStatus, searchF)
+    }
 
     const handleSearch = (e) => {
         setSearch(e.target.value);
@@ -237,6 +281,41 @@ export default function OrderList() {
                         }}>Найти</MyButton>
                     </div>
                 </Div>
+                <Div />
+                <Div>Фильтры:</Div>
+                <>
+                    <Li>
+                        <div className="flex items-center gap-x-3 flex-wrap">
+                            <div>Дата:</div>
+                            <div className="text-red">
+                                [
+                                <DataInput
+                                    color="red"
+                                    value={dates.startDate}
+                                    name="startDate"
+                                    change={handleDateChange}
+                                />
+                                ]
+                            </div>
+                            <div> - </div>
+                            <div className="text-red">
+                                [
+                                <DataInput
+                                    color="red"
+                                    value={dates.endDate}
+                                    name="endDate"
+                                    change={handleDateChange}
+                                />
+                                ]
+                            </div>
+                            <MyButton click={handleDate}>
+                                <span className="text-green-400">
+                                    Применить
+                                </span>
+                            </MyButton>
+                        </div>
+                    </Li>
+                </>
 
                 {userData?.role === "superAdmin" && <>
                     <Div />
--- client/src/Pages/OrdersWholeList.js
+++ client/src/Pages/OrdersWholeList.js
@@ -13,6 +13,15 @@ import clsx from "clsx";
 import OrderInfo from "../Components/OrderInfo";
 import useFetchUserData from "../customHooks/useFetchUserData";
 import useScrollPosition from "../customHooks/useScrollPosition";
+import DataInput from "../Components/DataInput";
+
+const getCurrentDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
 
 export default function OrdersWholeList() {
     const scrollPosition = useScrollPosition();
@@ -22,8 +31,8 @@ export default function OrdersWholeList() {
     const [searchF, setSearchF] = useState("");
     const [searchStatus, setSearchStatus] = useState(false);
     const [dates, setDates] = useState({
-        startDate: "",
-        endDate: "",
+        startDate: getCurrentDate(),
+        endDate: getCurrentDate(),
     });
     const [franchiseesModal, setFranchiseesModal] = useState(false);
     const [franchisee, setFranchisee] = useState(null);
@@ -64,6 +73,40 @@ export default function OrdersWholeList() {
         setFranchisee(chFranchisee);
         setFranchiseesModal(false);
     };
+
+    const handleDateChange = (e) => {
+        let input = e.target.value.replace(/\D/g, ""); // Remove all non-digit characters
+        if (input.length > 8) input = input.substring(0, 8); // Limit input to 8 digits
+
+        const year = input.substring(0, 4);
+        const month = input.substring(4, 6);
+        const day = input.substring(6, 8);
+
+        let formattedValue = year;
+        if (input.length >= 5) {
+            formattedValue += "-" + month;
+        }
+        if (input.length >= 7) {
+            formattedValue += "-" + day;
+        }
+
+        setDates({ ...dates, [e.target.name]: formattedValue });
+    };
+
+    const handleDate = () => {
+        if (dates.startDate.length !== 10 || dates.endDate.length !== 10) {
+            setOpen(true)
+            setStatus("error")
+            setMessage("Введите даты в формате ГГГГ-ММ-ДД")
+            return
+        }
+        
+        setOrders([]);
+        setPage(1);
+        setHasMore(true);
+        setLoading(false)
+        loadMoreOrders(1, dates, search, searchStatus, searchF)
+    }
 
     const handleSearch = (e) => {
         setSearch(e.target.value);
@@ -254,6 +297,42 @@ export default function OrdersWholeList() {
                         }}>Найти</MyButton>
                     </div>
                 </Div>
+
+                <Div />
+                <Div>Фильтры:</Div>
+                <>
+                    <Li>
+                        <div className="flex items-center gap-x-3 flex-wrap">
+                            <div>Дата:</div>
+                            <div className="text-red">
+                                [
+                                <DataInput
+                                    color="red"
+                                    value={dates.startDate}
+                                    name="startDate"
+                                    change={handleDateChange}
+                                />
+                                ]
+                            </div>
+                            <div> - </div>
+                            <div className="text-red">
+                                [
+                                <DataInput
+                                    color="red"
+                                    value={dates.endDate}
+                                    name="endDate"
+                                    change={handleDateChange}
+                                />
+                                ]
+                            </div>
+                            <MyButton click={handleDate}>
+                                <span className="text-green-400">
+                                    Применить
+                                </span>
+                            </MyButton>
+                        </div>
+                    </Li>
+                </>
 
                 {userData?.role === "superAdmin" && <>
                     <Div />
