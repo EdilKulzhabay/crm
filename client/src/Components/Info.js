5fc936c38d9cd0a1694c1c8014e14d48552ac93d
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2024-10-30 19:12:38 +0500

    30.10.2024 info

--- client/src/Components/Info.js
+++ client/src/Components/Info.js
@@ -1,6 +1,7 @@
 export default function Info(props) {
+    const ml = props.ml || "ml-3"
     return (
-        <span className="ml-3 text-red text-sm lg:text-base">
+        <span className={`text-red text-sm lg:text-base ${ml}`}>
             [ <span className="text-white">{props.children}</span> ]
         </span>
     );
