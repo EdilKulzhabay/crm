777519cf5361dc31cecaa7af4e139b3a4c88b3b6
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2024-10-30 18:42:11 +0500

    trhunfijdckmslfjnkd

--- client/src/Components/Li.js
+++ client/src/Components/Li.js
@@ -1,10 +1,13 @@
 import Div from "./Div";
+import Pagada from "../icons/Pagada";
 
 export default function Li(props) {
+    const icon = props.icon || false
     return (
         <Div>
             <div className="flex items-center gap-x-3">
-                <div className="h-px w-2 lg:w-3 bg-white"></div>
+                {icon ? <Pagada className="w-3 h-3" /> : <div className="h-px w-2 lg:w-3 bg-white" />}
+                
                 {props.children}
             </div>
         </Div>
