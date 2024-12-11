4bc2a858e1159cd0b4f67727480ae513971932a4
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2024-12-11 17:54:07 +0500

    11.12.2024 8

--- client/src/Components/Li.js
+++ client/src/Components/Li.js
@@ -7,9 +7,9 @@ export default function Li(props) {
     return (
         <Div>
             <div className="flex items-center gap-x-3">
-                {icon ? <Pagada className="w-[12px] h-[12px] shrink-0" /> : <div className="w-[12px] h-px shrink-0 bg-white" />}
+                {icon && <Pagada className="w-[12px] h-[12px] shrink-0" />}
                 {link === "verified" && <div className="w-[12px] h-px shrink-0 bg-white" />}
-                {(link === "waitingVerification" || link === "denyVerification") &&  <div className="text-xl -mt-1.5">{" "} x {" "}</div>}
+                {(link === "waitingVerification" || link === "denyVerification") && !icon &&  <div className="text-xl -mt-1.5">{" "} x {" "}</div>}
                 {props.children}
             </div>
         </Div>
