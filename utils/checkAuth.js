f027b59575b9c3e29b38bc70aabb57f0dd2afa23
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-01-05 19:00:25 +0500

    05.01.2024 5

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
     const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
-    console.log("token in checkAuth", token);
     
     if (token) {
         try {
