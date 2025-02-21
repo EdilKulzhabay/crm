374959b9f583a2e2702a6cf8a1eeca6563a5b85a
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-02-21 20:34:33 +0500

    21.02.2025 8

--- utils/checkAuthAggregator.js
+++ utils/checkAuthAggregator.js
@@ -1,32 +1,22 @@
 import jwt from "jsonwebtoken";
 
 export default async (req, res, next) => {
-    console.log("we in checkAuthAggregator");
-    
     const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
 
-    console.log("we in checkAuthAggregator2");
-    
     if (token) {
-        console.log("we in checkAuthAggregator3");
         try {
-            console.log("we in checkAuthAggregator4");
             const decoded = jwt.verify(token, process.env.SecretKey);
 
-            console.log("checkAuthAggregator deocded: ", decoded);
-            
-    
             req.userId = decoded?._id || null;
             req.role = decoded?.role || null; 
             next();
         } catch (e) {
-            console.log("we in checkAuthAggregator5");
             return res.status(403).json({
+                success: false,
                 message: "Нет доступа",
             });
         }
     } else {
-        console.log("we in checkAuthAggregator6");
         return res.status(403).json({
             success: false,
             message: "Нет доступа",
