f36b5ee857cdd2aa5d45881426f9d5e9fdd8aba7
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-06-07 15:11:35 +0500

    07.06.2025 2

--- utils/getLocationsLogic.js
+++ utils/getLocationsLogic.js
@@ -189,7 +189,7 @@ async function getLocationsLogic(orderId) {
         console.log("Заказ найден:", order);
 
         // Проверка и получение активных курьеров
-        const couriers = await CourierAggregator.find({ onTheLine: true, order: null });
+        const couriers = await CourierAggregator.find({ onTheLine: true });
         if (couriers.length === 0) {
             console.error("Ошибка: Нет активных курьеров");
             return;
