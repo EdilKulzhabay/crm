c31b230d7dde11e45e05d9641b734670c7ec8911
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-06-10 14:15:41 +0500

    10.06.2025 5

--- Controllers/CourierAggregatorController.js
+++ Controllers/CourierAggregatorController.js
@@ -500,7 +500,7 @@ export const completeOrderCourierAggregator = async (req, res) => {
             );
             console.log("CourierAggregatorController 487, отправили уведомление о заказе курьеру");
             
-            await new Promise(resolve => setTimeout(resolve, 30000));
+            await new Promise(resolve => setTimeout(resolve, 40000));
             const currentOrder = await Order.findById(courier.orders[0].orderId)
             if (currentOrder.status !== "onTheWay") {
                 await distributionUrgentOrder(courier.orders[0].orderId)
