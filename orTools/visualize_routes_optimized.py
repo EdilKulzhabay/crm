4183e4c11c9a97b52dcbe6361e81ed7f8542521f
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-07-22 12:43:04 +0500

    22.07.2025 5

--- orTools/visualize_routes_optimized.py
+++ orTools/visualize_routes_optimized.py
@@ -55,8 +55,8 @@ for active_order_data in active_orders_data.values():
     all_orders_for_viz.append(active_order_data)
 
 # Создаем словари для быстрого поиска (включая активные заказы)
-orders_dict = {order['id']: order for order in all_orders_for_viz}
-couriers_dict = {courier['id']: courier for courier in couriers}
+orders_dict = {order['_id']: order for order in all_orders_for_viz}
+couriers_dict = {courier['_id']: courier for courier in couriers}
 
 # Настройка графика
 plt.figure(figsize=(20, 16))
