53e3c932922cb867d61ddcdc0b4e3cccfcf46c1d
kulzhabai.e <kulzhabai.e@sailet.pro>
2025-12-10 10:55:23 +0500

    10.12.2025 1

--- Controllers/CourierAggregatorController.js
+++ Controllers/CourierAggregatorController.js
@@ -434,13 +434,15 @@ export const updateCourierAggregatorData = async (req, res) => {
         }
 
         if (changeField === "onTheLine" && changeData) {
-            const mail = "outofreach5569@gmail.com"
+            // const mail = "outofreach5569@gmail.com"
+            const mail = "edil.kulzhabay01@gmail.com"
             const sendText = `Курьер ${courier.fullName} появился в сети`
             sendEmailAboutAggregator(mail, "online", sendText)
         }
 
         if (changeField === "onTheLine" && !changeData) {
-            const mail = "outofreach5569@gmail.com"
+            // const mail = "outofreach5569@gmail.com"
+            const mail = "edil.kulzhabay01@gmail.com"
             const sendText = `Курьер ${courier.fullName} вышел из сети`
             sendEmailAboutAggregator(mail, "offline", sendText)
         }
@@ -851,7 +853,8 @@ export const cancelOrderCourierAggregator = async (req, res) => {
         //     }
         // }, 15000);
         
-        sendEmailAboutAggregator("outofreach5569@gmail.com", "cancelled", `Курьер ${courier.fullName} отменил заказ ${order.clientTitle}`)
+        // sendEmailAboutAggregator("outofreach5569@gmail.com", "cancelled", `Курьер ${courier.fullName} отменил заказ ${order.clientTitle}`)
+        sendEmailAboutAggregator("edil.kulzhabay01@gmail.com", "cancelled", `Курьер ${courier.fullName} отменил заказ ${order.clientTitle}`)
 
         try {
             const sendOrder = await Order.findById(order._id)
@@ -1793,7 +1796,8 @@ export const needToGiveTheOrderToCourier = async (req, res) => {
 
         const mailOptions = {
             from: "info@tibetskaya.kz",
-            to: "outofreach5569@gmail.com",
+            // to: "outofreach5569@gmail.com",
+            to: "edil.kulzhabay01@gmail.com",
             subject: `Нужно дать заказ курьеру ${fullName}`,
             text: `Нужно дать заказ курьеру ${fullName}`,
         };
