9ed014dbcad6c0a85e5d8d32f0e84684935a801f
kulzhabai.e <kulzhabai.e@sailet.pro>
2026-01-30 07:51:16 +0500

    30.01.2026 3

--- utils/migrateBClients.js
+++ utils/migrateBClients.js
@@ -1,19 +1,29 @@
+import mongoose from "mongoose";
 import Client from "../Models/Client.js";
 import Order from "../Models/Order.js";
+import "dotenv/config";
 
 const migrateBClients = async () => {
-    // 1) Найти уникальные clientId у заказов нужного franchisee
-    const franchiseeId = "66fc0cc6953c2dbbc86c2132";
+    try {
+        await mongoose.connect(process.env.MONGOURL);
 
-    const clientIds = await Order.distinct("client", { franchisee: franchiseeId });
+        // 1) Найти уникальные clientId у заказов нужного franchisee
+        const franchiseeId = new mongoose.Types.ObjectId("66fc0cc6953c2dbbc86c2132");
 
-    // 2) Получить список клиентов
-    const clients = await Client.find(
-        { _id: { $in: clientIds } },
-        { _id: 1, fullName: 1, phone: 1, mail: 1 }
-    ).populate("franchisee", "userName");
+        const clientIds = await Order.distinct("client", { franchisee: franchiseeId });
 
-    console.log(clients);
-}
+        // 2) Получить список клиентов
+        const clients = await Client.find(
+            { _id: { $in: clientIds } },
+            { _id: 1, fullName: 1, phone: 1, mail: 1 }
+        ).populate("franchisee", "userName");
+
+        console.log(JSON.stringify(clients, null, 2));
+    } catch (error) {
+        console.error("Ошибка при миграции:", error);
+    } finally {
+        await mongoose.disconnect();
+    }
+};
 
 migrateBClients();
